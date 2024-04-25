322255063cbfa769de5026add3c631d0a0759abf
unknown <abdulthetechguyf@gmail.com>
2024-04-25 13:06:50 +0100

    Second Commit WIth Splash Screen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Avengers 1.0, Lets goo!</Text>
       <StatusBar style="auto" />
     </View>
   );
